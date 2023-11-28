2b1440bb6603540b75eb076046da2bdf20a9dbb3
Jadegreen <Yichuan.Zhang22@student.xjtlu.edu.cn>
2023-11-28 20:37:14 +0800

    Day7 clear the cart

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,52 @@
+package com.sky.controller.user;
+
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+@Api(tags = "C port shopping cart interface")
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    @ApiOperation("add to the shopping cart")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("add to the shopping cart and the info is: {}",shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+
+
+    @GetMapping("/list")
+    @ApiOperation("check the shopping cart")
+    public Result<List<ShoppingCart>> list(){
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+        return Result.success(list);
+    }
+
+
+    /**
+     * clean the shopping cart
+     * @return
+     */
+    @DeleteMapping("/clean")
+    @ApiOperation("clear the shopping cart")
+    public Result clean(){
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
+
+}
